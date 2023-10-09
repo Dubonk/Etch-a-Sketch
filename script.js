da5b8a1b1dee6d05afe20dc6ee627da362334c41
kevin <kevindubon43@gmail.com>
2023-10-09 12:17:41 -0700

    Updated background colors and added eraser.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const small = document.querySelector('#small');
 const reset = document.querySelector('#reset');
 const tracks = document.querySelector('#tracks');
 const cells = document.createElement('div');
+const erase = document.querySelector('#erase');
 let isDrawing = false;
 
 
@@ -26,12 +27,15 @@ function baseCells(num) {
         cells.classList.toggle('outlineClicked');})
         const cells = document.createElement('div');
         cells.classList.add('cells');
-        cells.style.width = '15px';
-        cells.style.height = '15px';
+        cells.style.width = '13px';
+        cells.style.height = '13px';
         container.append(cells);
         cells.addEventListener('mousemove', () => { 
-            if(isDrawing){cells.style.background = "#000000";}
-        })}  
+            if(isDrawing && erase.classList.contains('clicked')){cells.style.background = "#d8d8d8";}
+            else if (isDrawing) {
+                cells.style.background = "black";
+            }
+        })   }  
     }
     
     if(num == 1024) {
@@ -41,11 +45,14 @@ function baseCells(num) {
         cells.classList.toggle('outlineClicked');})
         const cells = document.createElement('div');
         cells.classList.add('cells');
-        cells.style.width = '30px';
-        cells.style.height = '30px';
+        cells.style.width = '28px';
+        cells.style.height = '28px';
         container.append(cells);
         cells.addEventListener('mousemove', () => { 
-            if(isDrawing){cells.style.background = "#000000";}
+            if(isDrawing && erase.classList.contains('clicked')){cells.style.background = "#d8d8d8";}
+            else if (isDrawing) {
+                cells.style.background = "black";
+            }
         })}  
     }
     
@@ -59,7 +66,11 @@ function baseCells(num) {
         cells.classList.add('cells');
         container.append(cells);
         cells.addEventListener('mousemove', () => { 
-            if(isDrawing){cells.style.background = "#000000";}
+            if(isDrawing){
+                if(isDrawing && erase.classList.contains('clicked')){cells.style.background = "#d8d8d8";}
+                else if (isDrawing) {
+                    cells.style.background = "black";}
+                }
         })}  
     }
 
@@ -70,11 +81,15 @@ function baseCells(num) {
         cells.classList.toggle('outlineClicked');})
         const cells = document.createElement('div');
         cells.classList.add('cells');
-        cells.style.width = '120px';
-        cells.style.height = '120px';
+        cells.style.width = '118px';
+        cells.style.height = '118px';
         container.append(cells);
         cells.addEventListener('mousemove', () => {
-        if(isDrawing){cells.style.backgroundColor = "#000000" }});
+            if(isDrawing && erase.classList.contains('clicked')){cells.style.background = "#d8d8d8";}
+            else if (isDrawing) {
+                cells.style.background = "black";
+            }
+    });
 }
 }
 else if(num == 16) {
@@ -84,41 +99,53 @@ else if(num == 16) {
         cells.classList.toggle('outlineClicked');})
         const cells = document.createElement('div');
         cells.classList.add('cells');
-        cells.style.width = '240px';
-        cells.style.height = '240px';
+        cells.style.width = '238px';
+        cells.style.height = '238px';
         container.append(cells);
         cells.addEventListener('mousemove', () => {
-        if(isDrawing){cells.style.backgroundColor = "#000000";}
+        if(isDrawing){
+            if(isDrawing && erase.classList.contains('clicked')){cells.style.background = "#d8d8d8";}
+            else if (isDrawing) {
+                cells.style.background = "black";}
+            }
     })
 }}
+
 }
 
 small.addEventListener('click', () => {
+    deleteCells();
     baseCells(16);
-})
+});
 eight.addEventListener('click', () => {
+    deleteCells();
     baseCells(64);
-})
+});
 sixteen.addEventListener('click', () => {
+    deleteCells();
     baseCells(256);
-})
+});
 large.addEventListener('click', () => {
+    deleteCells();
     baseCells(1024);
-})
+});
 largest.addEventListener('click', () => {
+    deleteCells();
     baseCells(4096);
-})
+});
 reset.addEventListener('click', () => {
-    deleteCells('cells');
+    deleteCells();
     baseCells(256);
-})
-
-
-
+});
+erase.addEventListener('click', () => {
+    erase.classList.toggle('clicked');
+});
 
 function deleteCells() {
     const elements = document.getElementsByClassName('cells');
    while(elements.length > 0) {
     elements[0].parentNode.removeChild(elements[0]);
    }
-}
+};
+
+baseCells(256);
